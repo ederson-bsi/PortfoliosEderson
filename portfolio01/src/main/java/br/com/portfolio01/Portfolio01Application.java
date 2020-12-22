b7fd8d2d063cd2e4a4e63ba2f1163345383e0aa2
Ederson <ederson.bsi@gmail.com>
2020-12-22 16:05:45 -0300

    Primeiro commit

---
+++ portfolio01/src/main/java/br/com/portfolio01/Portfolio01Application.java
@@ -0,0 +1,13 @@
+package br.com.portfolio01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Portfolio01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Portfolio01Application.class, args);
+	}
+
+}
